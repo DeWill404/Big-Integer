0eeca3e2a3c75bbe28681263776981295d1bc123
DeWill404 <mr.irshad.siddique@gmail.com>
2020-07-26 22:29:48 +0530

    Refactor

--- BigInt.c
+++ BigInt.c
@@ -3,8 +3,24 @@
 #include <ctype.h>
 
 #include "Bigint_basic.c"
+#include "Bigint_arithmatic.c"
 
-int bigvalid(Bigint number) { Bigint_isNone(number); }
-Bigint bignull() { Bigint_none(); }
-Bigint bigset(char *string) { Bigint_set(string); }
-int bigcmp(Bigint n1, Bigint n2) { Bigint_compare(n1, n2); }
+int bigvalid(Bigint number) { 
+	return Bigint_isValid(number); 
+}
+
+Bigint bignull(char msg[]) { 
+	return Bigint_none(msg); 
+}
+
+Bigint bigset(char *string) {
+	return Bigint_set(string); 
+}
+
+int bigcmp(Bigint n1, Bigint n2) { 
+	return Bigint_compare(n1, n2); 
+}
+
+Bigint bigadd(Bigint n1, Bigint n2) {
+	return Bigint_add(n1, n2);
+}
